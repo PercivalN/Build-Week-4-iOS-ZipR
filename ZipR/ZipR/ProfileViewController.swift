7eb4fd472f807285b3e78f0adcb10e5df839c7a4
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-02-03 10:10:36 -0800

    Connected storyboard to classes.

--- ZipR/ZipR/ProfileViewController.swift
+++ ZipR/ZipR/ProfileViewController.swift
@@ -13,6 +13,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.title = "Profile"
         // Do any additional setup after loading the view.
     }
     
